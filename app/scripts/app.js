ed26b3f9a39597861780b2e90c9658d87c5bf0c9
Alexander Thohoh <jerk401@gmail.com>
2016-02-28 16:21:16 +0300

    updated

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,6 +20,7 @@ angular
       })
       .state('register', {
         url: '/register',
-        templateUrl: 'views/register.html'
+        templateUrl: 'views/register.html',
+        controller: 'RegisterCtrl'
       })
   });
--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -8,10 +8,19 @@
  * Controller of the authicationAngularApp
  */
 angular.module('authicationAngularApp')
-  .controller('RegisterCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('RegisterCtrl', function ($scope, $http) {
+    $scope.submit = function () {
+
+
+      var url = '/';
+      var user = {};
+
+      $http.post(url, user)
+        .success(function (res) {
+          console.log('success');
+        })
+        .error(function (err) {
+          console.log('error');
+        });
+    }
   });
